ea2562689ea83c2d3e32553b916f2a09b17aff7a
mahdialenezi <mahdialmanhal32@gmail.com>
2024-11-06 16:01:31 +0300

    task

---
+++ functions1.js
@@ -0,0 +1,63 @@
+// task1
+
+
+const printName = () => {
+    console.log("Hello, MAHDI!");
+  };
+  
+  // printName();
+
+
+
+
+// task2
+
+// function printage(birthyear)  {
+//     console.log('you are ${2024 - birthyear}.');
+//   };
+
+
+// printage(1983);
+
+
+// task3
+
+
+function printageandname(birthyear, name)  {
+  console.log('Hello ${name} you are ${2024 - birthyear} year old.');
+};
+
+
+// printageandname(1983, "mahdi");
+
+
+// task4
+
+// function printHello(name, language)  {
+//   if (language === "en") {
+//     console.log('Hello ${mahdi}'); 
+//   } else if (language === "es") {
+//     console.log('Hola ${mahdi}');
+//   } else if (language === "fr") {
+//     console.log('Bonjour ${mahdi}');
+//   } else if (language === "tr") {
+//     console.log('marhaba ${mahdi}');
+
+// }
+
+
+// printHello("mahdi", "fr");
+
+
+
+// task5
+
+function printmax(firstnumber, secondnumber)  {
+  if (firstnumber > secondnumber) {
+    console.log(firstnumber); 
+  } else {
+    console.log(secondnumber); 
+  } 
+
+}
+printmax(2, 9);
